760f7ebe36b7f5e5366ffba9adb64adeb4179e4d
shadowjura82 <shadowjura82@gmail.com>
2024-02-04 21:53:53 +0100

    Testing

--- src/main/java/TestingProject/tst/services/PropertiesService.java
+++ src/main/java/TestingProject/tst/services/PropertiesService.java
@@ -1,18 +1,16 @@
 package TestingProject.tst.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PropertiesService {
+    @Value("${first.string.variable}")
     private String stringFirst;
+    @Value("${second.string.variable}")
     private String stringSecond;
+    @Value("${flag.variable}")
     private Integer flag;
-
-    public PropertiesService() {
-        this.stringFirst = "This is first string";
-        this.stringSecond = "This is second string";
-        this.flag = 10;
-    }
 
     public String getString() {
         if (flag < 20) {
